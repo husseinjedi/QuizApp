23bf2c3dde1bf4f55b7a8b1795fd0b3a6b4e8311
HusseinRashy10 <139256631+husseinjedi@users.noreply.github.com>
2023-07-21 08:43:55 +0100

    Commiting my quiz app
    Enjoy it

---
+++ js/questions.js
@@ -0,0 +1,193 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Which animal is the biggest animal in the world?",
+    answer: "Antarctic blue whale",
+    options: [
+      "Antarctic blue whale",
+      "African Elephant",
+      "White Rhinoceros",
+      "North Pacific Right Whale"
+    ]
+  },
+   {
+    numb: 7,
+    question: "What does wwe stand for?",
+    answer: "World Wrestling Entertainment",
+    options: [
+      "Wide Wrestling Enterprise",
+      "World Wrestling Entertainment",
+      "Web Wrestling Entertaining",
+      "World Wrestling Enjoyment"
+    ]
+  },
+     {
+    numb: 8,
+    question: "What does json stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Object Note",
+      "JavaScript Objects Notification",
+      "JavaScript Object Notation",
+      "Java Stylish Object Notation"
+    ]
+  },
+   {
+    numb: 9,
+    question: "What does lwo stand for?",
+    answer: "Latino World Order",
+    options: [
+      "Latino World Order",
+      "Latino Wide Order",
+      "Little Web Ordering",
+      "Latino wed Organising"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How many trophies has pele won?",
+    answer: "26",
+    options: [
+      "34",
+      "26",
+      "40",
+      "42"
+    ]
+  },
+    {
+    numb: 11,
+    question: "How many trophies has Ibrahimović won?",
+    answer: "34",
+    options: [
+      "34",
+      "33",
+      "36",
+      "31"
+    ]
+  }, 
+    {
+    numb: 12,
+    question: "How many trophies has Pique won?",
+    answer: "37",
+    options: [
+      "40",
+      "32",
+      "37",
+      "39"
+    ]
+  }, 
+    {
+    numb: 13,
+    question: "How many trophies has Rooney won?",
+    answer: "13",
+    options: [
+      "13",
+      "20",
+      "22",
+      "10"
+    ]
+  }, 
+   {
+    numb: 14,
+    question: "How many trophies has sir alex ferguson won as a manager?",
+    answer: "49",
+    options: [
+      "39",
+      "35",
+      "49",
+      "50"
+    ]
+  }, 
+    {
+    numb: 15,
+    question: "How many trophues has Alfredo di Stéfano won as a player ?",
+    answer: "18",
+    options: [
+      "17",
+      "23",
+      "20",
+      "18"
+    ]
+  }, 
+   {
+    numb: 16,
+    question: "How many trophues has Pep Guardiola won as a player ?",
+    answer: "35",
+    options: [
+      "30",
+      "36",
+      "35",
+      "32"
+    ]
+  },  
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+  ];
